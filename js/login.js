85e5bd8ec1830727ffecd2c7a4f22cfb90fae522
Samuli Lamminmäki <samuli.lamminmaki@metropolia.fi>
2024-03-30 23:30:34 +0200

    initial commit

---
+++ js/login.js
@@ -0,0 +1,26 @@
+
+async function createUser(){
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+
+
+    const response = await fetch("https://10.120.32.94/restaurant/api/v1/users", {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username,
+            password: password,
+            email: email
+        })
+    });
+    const data = await response.json();
+    console.log(data.message)
+
+
+    if(response.status === 200){
+        console.log('user successfully created!')
+    }
+}
