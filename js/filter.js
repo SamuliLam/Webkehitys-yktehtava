581203503982731bbf0694048273fad66ebc69d7
Samuli Lamminmäki <samuli.lamminmaki@metropolia.fi>
2024-04-26 12:44:49 +0300

    siivousta

--- js/filter.js
+++ js/filter.js
@@ -2,18 +2,14 @@ import * as utils from './utils.js';
 import { displayRestaurants } from './restaurant-list.js';
 
 document.getElementById('name-filter').addEventListener('click', function () {
-    // displayRestaurants('name');
     this.querySelector('.triangle').classList.toggle('rotated');
 });
 
 document.getElementById('city-filter').addEventListener('click', function () {
-    // displayRestaurants('city');
     this.querySelector('.triangle').classList.toggle('rotated');
 });
 
-// The favorite section does not do anything yet
 document.getElementById('favorite-filter').addEventListener('click', function () {
-    // TODO: Implement favorite filtering
     this.querySelector('.triangle').classList.toggle('rotated');
 });
 
